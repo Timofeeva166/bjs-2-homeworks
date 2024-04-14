09e2257de1ce2899df40a9b999334df4a3afc42a
Julia Timofeeva <J-Timofeeva166@yandex.ru>
2024-04-14 23:52:41 +0300

    Task Objects added

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  }
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  };
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+  Student.prototype.addMarks = function (...marksToAdd) {
+    if (this.marks !== undefined) {
+      this.marks.push(...marksToAdd);
+    }
+  };
   
-}
-
-Student.prototype.getAverage = function () {
+  Student.prototype.getAverage = function () {
+    if (this.marks === 0 || this.marks.length == undefined) {
+      return 0;
+    } else {
+      let result = this.marks.reduce((acc, item, index, arr) => {
+        let sum = acc + item;
+        if (index === arr.length - 1) {
+          return sum / arr.length;
+        }
+        return sum;
+      }, 0);
+      return result;
+    }
+  };
   
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+  Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  };
